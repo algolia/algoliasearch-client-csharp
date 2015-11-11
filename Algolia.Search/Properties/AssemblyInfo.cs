2482b9164d93d4efb455a1f12c02d3c94fd0930c
Xavier Grand <grand.xavier@gmail.com>
2015-11-11 15:46:42 +0100

    Bump to 3.4.2

--- Algolia.Search/Properties/AssemblyInfo.cs
+++ Algolia.Search/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 
 internal class AssemblyInfo
 {
-    public const string AssemblyVersion = "3.4.1";
-    public const string AssemblyFileVersion = "3.4.1";
+    public const string AssemblyVersion = "3.4.2";
+    public const string AssemblyFileVersion = "3.4.2";
 }
