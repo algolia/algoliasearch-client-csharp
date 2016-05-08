d4de5301e801a5d2312c5c6d7f6ba29a042873fc
Baptiste Coquelle <coquellebaptiste@gmail.com>
2016-05-08 21:55:23 +0200

    shuffling read and write hosts (#45)
    * shuffling read and write hosts
    
    Signed-off-by: CBaptiste <coquellebaptiste@gmail.com>
    
    * adding comment

---
+++ Algolia.Search/Utils/ArrayHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algolia.Search.Utils
+{
+    public class ArrayUtils<T>
+    {
+        Random _rand;
+        public ArrayUtils()
+        {
+            _rand = new Random();
+        }
+
+        public IEnumerable<T> Shuffle(IEnumerable<T> toBeShuffled)
+        {
+            var randomList = new List<KeyValuePair<int, T>>();
+
+            foreach(var el in toBeShuffled)
+            {
+                randomList.Add(new KeyValuePair<int, T>(_rand.Next(), el));
+            }
+
+            T[] result = new T[toBeShuffled.Count()];
+            int index = 0;
+            foreach (KeyValuePair<int, T> pair in randomList.OrderBy(x => x.Key))
+            {
+                result[index] = pair.Value;
+                index++;
+            }
+            return result;
+        }
+    }
+}
