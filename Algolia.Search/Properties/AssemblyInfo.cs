bb351a5dfc1c2c9aa34adead15a2bb462f8a8c4e
CBaptiste <coquellebaptiste@gmail.com>
2016-10-19 17:03:47 +0200

    'bump'

--- Algolia.Search/Properties/AssemblyInfo.cs
+++ Algolia.Search/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 
 internal class AssemblyInfo
 {
-    public const string AssemblyVersion = "3.6.6";
-    public const string AssemblyFileVersion = "3.6.6";
+    public const string AssemblyVersion = "3.6.7";
+    public const string AssemblyFileVersion = "3.6.7";
 }
