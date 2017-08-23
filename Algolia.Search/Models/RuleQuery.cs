4677f5335091fd94921000a74529fd3492c35cec
Guy Daher <guydaher1@gmail.com>
2017-08-23 08:28:28 -0700

    feat(query-rules): implementation of 5 methods related to query rules, both sync and async

---
+++ Algolia.Search/Models/RuleQuery.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Algolia.Search.Models
+{
+    public class RuleQuery
+	{
+		public RuleQuery() { }
+
+        public RuleQuery(string query, string anchoring, string context)
+        {
+            Query = query;
+            Anchoring = anchoring;
+            Context = context;
+        }
+
+        public RuleQuery(string query, string anchoring, string context, int page, int hitsPerPage)
+        {
+            Query = query;
+            Anchoring = anchoring;
+            Context = context;
+            Page = page;
+            HitsPerPage = hitsPerPage;
+        }
+
+	    public string Query { get; set; } = "";
+	    public string Anchoring { get; set; } = "";
+	    public string Context { get; set; } = "";
+
+	    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public int? Page { get; set; }
+
+	    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public int? HitsPerPage { get; set; }
+    }
+
+}
