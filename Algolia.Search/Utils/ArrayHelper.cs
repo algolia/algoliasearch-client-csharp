d49e9875dbd603ab06c78f2160e7cf6ed1b63216
Chris McKee <chris.mckee@ivendi.com>
2018-07-31 11:40:47 +0100

    Remove redundant code from shuffle (tested shuffle https://dotnetfiddle.net/JB5uGQ)

--- Algolia.Search/Utils/ArrayHelper.cs
+++ Algolia.Search/Utils/ArrayHelper.cs
@@ -6,21 +6,13 @@ namespace Algolia.Search.Utils
 {
     public static class EnumerableExtensions
     {
+        static Random random = new Random();
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
         {
-            return source.Shuffle(new Random());
+            return source.ShuffleIterator(random);
         }
 
-        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random rng)
-        {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-            if (rng == null) throw new ArgumentNullException(nameof(rng));
-
-            return source.ShuffleIterator(rng);
-        }
-
-        private static IEnumerable<T> ShuffleIterator<T>(
-            this IEnumerable<T> source, Random rng)
+        private static IEnumerable<T> ShuffleIterator<T>(this IEnumerable<T> source, Random rng)
         {
             var buffer = source.ToList();
             for (int i = 0; i < buffer.Count; i++)
