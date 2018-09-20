0be45ae56625982f2a046d002005c72332b8aea3
Antoine Hémery <antoine.hemery@algolia.com>
2018-09-20 09:40:27 +0200

    Added Enabled flag feature

--- Algolia.Search/Models/RuleQuery.cs
+++ Algolia.Search/Models/RuleQuery.cs
@@ -3,41 +3,44 @@ using Newtonsoft.Json;
 
 namespace Algolia.Search.Models
 {
-	public class RuleQuery
-	{
-		public RuleQuery()
-		{
-		}
+    public class RuleQuery
+    {
+        public RuleQuery()
+        {
+        }
 
-		public RuleQuery(string query, string anchoring, string context)
-		{
-			Query = query;
-			Anchoring = anchoring;
-			Context = context;
-		}
+        public RuleQuery(string query, string anchoring, string context)
+        {
+            Query = query;
+            Anchoring = anchoring;
+            Context = context;
+        }
 
-		public RuleQuery(string query, string anchoring, string context, int page, int hitsPerPage)
-		{
-			Query = query;
-			Anchoring = anchoring;
-			Context = context;
-			Page = page;
-			HitsPerPage = hitsPerPage;
-		}
+        public RuleQuery(string query, string anchoring, string context, int page, int hitsPerPage)
+        {
+            Query = query;
+            Anchoring = anchoring;
+            Context = context;
+            Page = page;
+            HitsPerPage = hitsPerPage;
+        }
 
-		[JsonProperty(PropertyName = "query")]
-		public string Query { get; set; } = "";
+        [JsonProperty(PropertyName = "query")]
+        public string Query { get; set; } = "";
 
-		[JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "anchoring")]
-		public string Anchoring { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "anchoring")]
+        public string Anchoring { get; set; }
 
-		[JsonProperty(PropertyName = "context")]
-		public string Context { get; set; } = "";
+        [JsonProperty(PropertyName = "context")]
+        public string Context { get; set; } = "";
 
-		[JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "page")]
-		public int? Page { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "page")]
+        public int? Page { get; set; }
 
-		[JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "hitsPerPage")]
-		public int? HitsPerPage { get; set; }
-	}
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "hitsPerPage")]
+        public int? HitsPerPage { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "enabled")]
+        public bool? Enabled { get; set; }
+    }
 }
