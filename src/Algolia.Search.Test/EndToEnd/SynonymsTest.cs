b4f091b4bdcfe744242282aa17e4d24c3a5c946c
Antoine Hémery <antoine.hemery@algolia.com>
2019-01-06 19:13:01 +0100

    Fix: bug in synonymsTest

--- src/Algolia.Search.Test/EndToEnd/SynonymsTest.cs
+++ src/Algolia.Search.Test/EndToEnd/SynonymsTest.cs
@@ -63,7 +63,7 @@ namespace Algolia.Search.Test.EndToEnd
         [Test]
         public async Task SynonymsOperationsTest()
         {
-            BatchIndexingResponse addObjectResponse = await _index.SaveObjectsAsync(_objectsToSave);
+            BatchIndexingResponse addObjectResponse = await _index.SaveObjectsAsync(_objectsToSave, autoGenerateObjectId: true);
             addObjectResponse.Wait();
 
             Synonym gba = new Synonym
