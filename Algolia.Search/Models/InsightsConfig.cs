fc1f09e624a2728a8c2f47aaff72ac090336feaf
Antoine Hémery <antoine.hemery@algolia.com>
2019-01-16 13:45:08 +0100

    Removed: Legacy files

--- Algolia.Search/Models/InsightsConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace Algolia.Search.Models
-{
-    public class InsightsConfig
-    {
-        public string AppId { get; set; }
-        public string ApiKey { get; set; }
-        public string Region { get; set; } = "us";
-    }
-}
