cfebcf0fe3044e821542876b8678bd526358995c
Antoine Hémery <antoine.hemery@algolia.com>
2019-04-26 20:08:35 +0200

    Removed: unused IRetryStrategy

--- src/Algolia.Search/Transport/RetryStrategy.cs
+++ src/Algolia.Search/Transport/RetryStrategy.cs
@@ -1,25 +1,25 @@
 /*
-* Copyright (c) 2018 Algolia
-* http://www.algolia.com/
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*/
+ * Copyright (c) 2018 Algolia
+ * http://www.algolia.com/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 using Algolia.Search.Clients;
 using Algolia.Search.Models.Enums;
@@ -29,13 +29,12 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Algolia.Search.Test")]
-
 namespace Algolia.Search.Transport
 {
     /// <summary>
     /// Retry strategy logic in case of error/timeout
     /// </summary>
-    internal class RetryStrategy : IRetryStrategy
+    internal class RetryStrategy
     {
         /// <summary>
         /// Hosts that will be used by the strategy
@@ -58,7 +57,10 @@ namespace Algolia.Search.Transport
             _hosts = config.CustomHosts ?? config.DefaultHosts;
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Returns the tryable host regarding the retry strategy
+        /// </summary>
+        /// <param name="callType">The Call type <see cref="Models.Enums.CallType"/></param>
         public IEnumerable<StatefulHost> GetTryableHost(CallType callType)
         {
             lock (_lock)
@@ -81,7 +83,13 @@ namespace Algolia.Search.Transport
             }
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Update host's state
+        /// </summary>
+        /// <param name="tryableHost">A statefull host</param>
+        /// <param name="httpResponseCode">An http response code</param>
+        /// <param name="isTimedOut">Is the request timed out</param>
+        /// <returns></returns>
         public RetryOutcomeType Decide(StatefulHost tryableHost, int httpResponseCode, bool isTimedOut)
         {
             lock (_lock)
@@ -128,7 +136,7 @@ namespace Algolia.Search.Transport
         private bool IsRetryable(int httpResponseCode)
         {
             return (int)Math.Floor((decimal)httpResponseCode / 100) != 2 &&
-                   (int)Math.Floor((decimal)httpResponseCode / 100) != 4;
+                (int)Math.Floor((decimal)httpResponseCode / 100) != 4;
         }
 
         /// <summary>
